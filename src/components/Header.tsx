454cf10d4cf0a4101c608e7ecc3fb1f2d9aac994
Lucas Silva <lucaslisilva@outlook.com>
2023-01-26 08:54:29 -0300

    add two new components and some configs default

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { View, TouchableOpacity } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import colors from 'tailwindcss/colors'
+
+import Logo from '../assets/logo.svg'
+export function Header() {
+  return (
+    <View className='w-full flex-row items-center justify-between'>
+      <Logo />
+      <TouchableOpacity
+        activeOpacity={0.7}
+      >
+        <Feather
+          name="plus"
+          color={colors.violet[500]}
+          size={20}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
